079a1143143a3fe8118f7cb3ebffef608fbd7b3d
ALIENMEKO\ohmek <ohmeko.ocampo@gmail.com>
2026-03-02 19:33:49 -0800

    Continuing new Python Book: Python Crash Course 3rd Edition. Currently on Chapter 3

---
+++ PythonCrashCourse3rdEdition_1_24_2026/Chapter3_Lists/GuestList.py
@@ -0,0 +1,5 @@
+guest_list = ["Twilight Sparkle", "Toshino Kyouko", "Shana"]
+
+for guest in guest_list:
+    print(f"You are invited to my party: {guest}. Please come.")
+    
---
+++ PythonCrashCourse3rdEdition_1_24_2026/names.py
@@ -0,0 +1,13 @@
+names = ["Billy", "Bob", "Elmo"]
+
+for name in names:
+    print(f"My friends name is: {name}")
+
+for name in names:
+    print(f"Hello! {name}")
+
+vehicles = ["Honda", "Tesla","Toyota"]
+
+print(f"I own a {vehicles[0]}")
+print(f"I would like to own a {vehicles[1]}")
+print(f"I don't own a {vehicles[2]}")
